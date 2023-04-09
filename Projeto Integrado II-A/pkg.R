5e1612f1d948a66911710ffcfcf1663fb424e466
LuizPaulo23 <luizpauloueg@gmail.com>
2023-04-08 23:22:21 -0300

    ADD: dependências e pacotes utilizados

---
+++ Projeto Integrado II-A/pkg.R
@@ -0,0 +1,11 @@
+#' @title Pacotes/Dependências
+#' @author Luiz Paulo T. Gonçalves 
+#' @description
+#' 
+
+library(shiny)
+library(tidyverse)
+library(ggplot2)
+library(DataExplorer)
+library(redux)
+library(readxl)
